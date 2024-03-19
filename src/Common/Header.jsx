1f5c4317cf773ff0e18831077363894f2ea9473c
sultanBytes <sultankhand11@gmail.com>
2024-03-19 13:19:49 +0530

    first commit

---
+++ src/Common/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css";
+import logo from '../images/image 134 (1).png';
+
+const Header = () => {
+  return (
+    <header>
+        <img src={logo} />
+    </header>
+  )
+}
+
+export default Header
